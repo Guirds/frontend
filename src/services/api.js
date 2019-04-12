18b7c55849622f84b0e5dc9bf830b442cd3d8e70
Guilherme Santos <gui07015@gmail.com>
2019-04-12 11:39:43 -0300

    Aplicação frontend

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+import axios from 'axios';
 
 const api = axios.create({
     baseURL: 'https://omnistack-backend.herokuapp.com', // minha api node: 'https://omnistack6-0.herokuapp.com'
